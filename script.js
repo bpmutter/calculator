aa02b6398adecdf520d5070b64628bf2732c1cfd
Ben Perlmutter <benperlmutter94@gmail.com>
2019-12-27 10:39:26 -0500

    working calculator except when using multiple operators

--- script.js
+++ script.js
@@ -15,27 +15,59 @@ let divide = function (a, b) {
 
 //general function to call the above operators
 let operate = function (a, b, operator) {
-  return operator(a, b);
+  if (operator == "add") return add(a, b);
+  if (operator == "subtract") return subtract(a, b);
+  if (operator == "multiply") return multiply(a, b);
+  if (operator == "divide") return divide(a, b);
+  else return console.log("ERROR with operator");
+
 }
 
 const displayContent = document.getElementById("displayNumber");
 //display value on the screen
-let displayValue = function () {
-  let displayButtons = document.querySelectorAll('.numButton');
-
-  for (const button of displayButtons) {
-    button.addEventListener("click", () => {
-      displayContent.innerHTML = displayContent.innerHTML + button.innerHTML;
-    })
-  }
-
+let displayButtons = document.querySelectorAll('.numButton');
+for (const button of displayButtons) {
+  button.addEventListener("click", () => {
+    displayContent.innerHTML += button.innerHTML;
+  });
 }
+let operatorName; //set operator name to be used in compute function below
+const operators = document.querySelectorAll(".operator");
+for (let operator of operators) {
+  operator.addEventListener("click", () => {
+    operatorName = operator.name;
+    displayContent.innerHTML += operator.innerHTML;
+  });
+}
+
+let compute = document.getElementById("equals");
+compute.addEventListener("click", () => {
+  const re = /\*|\+|\/|-/;
+  const contentArr = displayContent.innerHTML.split(re);
+
+  let num1 = contentArr[0];
+  let num2 = contentArr[1];
+
+  displayContent.innerHTML = operate(num1, num2, operatorName);
+
+});
+
+
 let clearDisplay = function () {
   const clearButton = document.getElementById("clear");
   clearButton.addEventListener("click", () => {
     displayContent.innerHTML = "";
-  })
+  });
 }
 
-displayValue();
+let deleteVal = function () {
+  const deleteButton = document.getElementById("delete");
+  deleteButton.addEventListener("click", () => {
+    displayContent.innerHTML = displayContent.innerHTML.substring(0,
+      displayContent.innerHTML.length - 1);
+  });
+
+}
+
 clearDisplay();
+deleteVal();
